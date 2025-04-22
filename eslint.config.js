d06eabb261ee9aef835a8470116bf02b6c9b5d06
DongGyun <wooheuy5752@gmail.com>
2025-04-22 10:38:10 +0900

    feat: eslint, prettier, tailwind 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,89 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: { version: '18.3' },
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx'],
+        },
+        alias: {
+          map: [['~', '@']],
+          extensions: ['.js', '.jsx'],
+        },
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
+      'unused-imports': eslintPluginUnusedImports,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      import: eslintPluginImport,
+      'jsx-a11y': eslintPluginJsxA11y,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'react/prop-types': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-uses-react': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
         'warn',
-        { allowConstantExport: true },
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-duplicate-imports': 'off',
+      'import/newline-after-import': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['@/(.*)'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
       ],
     },
   },
-)
+]
