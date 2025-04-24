80deffbed8cfcc33b67149521b9e0bd12d325883
DongGyun <wooheuy5752@gmail.com>
2025-04-24 16:33:49 +0900

    feat: Loading 구현

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,10 @@
+import { getCssVar } from '@/utils/getCssVar'
 import { SyncLoader } from 'react-spinners'
 
 export const Loading = () => {
   return (
-    <div className={`flex h-full w-full flex-row items-center justify-center`}>
-      <SyncLoader color="#6db0f4" />
+    <div className={`flex-center flex h-full w-full`}>
+      <SyncLoader color={getCssVar('--color-primary')} />
     </div>
   )
 }
