e57cfa9bf7f648b9286c52c6427ddbdf3dd457c4
DongGyun <wooheuy5752@gmail.com>
2025-04-24 15:59:53 +0900

    feat: 버튼 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,69 +1,36 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
-import { ArrowRightIcon } from './icons/PropIcons'
+
+type ButtonSize = 'lg' | 'md' | 'sm'
 
 type Props = {
-  size: 'lg' | 'md' | 'sm'
+  size: ButtonSize
   secondary?: boolean
-  withArrow?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-const SIZE_STYLES = {
-  lg: {
-    padding: 'py-[2rem] px-[1.8rem]',
-    rounded: 'rounded-[2rem]',
-    font: 'text-p5',
-  },
-  md: {
-    padding: 'py-[1.4rem] px-[1.6rem]',
-    rounded: 'rounded-[1.6rem]',
-    font: 'text-p7',
-  },
-  sm: {
-    padding: 'py-[1rem] px-[1.4rem]',
-    rounded: 'rounded-[1.2rem]',
-    font: 'text-p8',
-  },
+const BUTTON_STYLES: Record<ButtonSize, string> = {
+  lg: 'p3 px-[1.8rem] py-[2rem] rounded-[2rem]',
+  md: 'p4 px-[1.6rem] py-[1.4rem] rounded-2xl',
+  sm: 'p5 px-[1.4rem] py-[1rem] rounded-xl',
 }
-
-const BaseButton = ({ size, withArrow, className, children }: PropsWithChildren<Props>) => {
-  const buttonStyle = withArrow ? 'flex items-center gap-5' : 'inline-block'
-
-  return (
-    <button
-      className={`shrink-0 text-white transition-all ${buttonStyle} ${className} ${SIZE_STYLES[size].font} ${SIZE_STYLES[size].rounded} ${SIZE_STYLES[size].padding}`}
-    >
-      {children}
-    </button>
-  )
-}
-
-const PrimaryButton = (props: PropsWithChildren<Props>) => (
-  <BaseButton {...props} className="bg-blue-500 hover:bg-blue-600">
-    {props.children}
-  </BaseButton>
-)
-
-const SecondaryButton = (props: PropsWithChildren<Props>) => (
-  <BaseButton {...props} className="bg-grey-500">
-    {props.children}
-  </BaseButton>
-)
 
 export const Button = ({
   size,
-  disabled = false,
-  type = 'button',
   secondary = false,
-  withArrow = false,
+  disabled = false,
+  className,
   children,
   ...rest
 }: PropsWithChildren<Props>) => {
-  const ButtonComponent = secondary || disabled ? SecondaryButton : PrimaryButton
+  const buttonStyle =
+    secondary || disabled ? 'bg-grey hover:bg-[#747476]' : 'bg-primary hover:bg-black'
 
   return (
-    <ButtonComponent type={type} size={size} disabled={disabled} withArrow={withArrow} {...rest}>
+    <button
+      disabled={disabled}
+      className={`text-light-text shrink-0 transition-all ${className} ${buttonStyle} ${BUTTON_STYLES[size]}`}
+      {...rest}
+    >
       {children}
-      {withArrow && <ArrowRightIcon size={size} />}
-    </ButtonComponent>
+    </button>
   )
 }
