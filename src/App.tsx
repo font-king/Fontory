70893a747328b6f9958343abff20b5f52232fc4f
DongGyun <wooheuy5752@gmail.com>
2025-04-27 22:46:58 +0900

    feat: 프로필 카드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toast } from './app/provider/ToastContainer'
 import { RouterComponent } from './app/router/RouterComponent'
 import { Header } from './components/Header'
 import { Loading } from './components/Loading'
+import { ProfileCard } from './components/ProfileCard'
 import { ProgressCard } from './components/ProgressCard'
 import { SideNavigationBar } from './components/SideNavigationBar'
 
@@ -17,7 +18,7 @@ function App() {
       <div className="flex-column bg-background min-h-dvh">
         <Header />
 
-        <main className="grid flex-grow grid-cols-[1fr_4fr_1fr] gap-4 overflow-hidden px-8 py-4">
+        <main className="grid flex-grow grid-cols-[4fr_15fr_5fr] gap-4 overflow-hidden px-8 py-4">
           <SideNavigationBar />
 
           <ErrorBoundary FallbackComponent={() => <div></div>}>
@@ -28,7 +29,8 @@ function App() {
 
           <ErrorBoundary FallbackComponent={() => <div></div>}>
             <Suspense fallback={<Loading />}>
-              <div>
+              <div className="flex-column gap-10">
+                <ProfileCard />
                 <ProgressCard />
               </div>
             </Suspense>
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,24 @@
+import { ProfileImage } from './ProfileImage'
+import { SectionLayout } from './SectionLayout'
+
+export const ProfileCard = () => {
+  const fontName = '고로케'
+  const count = 32
+
+  return (
+    <SectionLayout>
+      <div className="flex-between-center px-4">
+        <ProfileImage size="md" />
+        <div className="flex-column items-center gap-3 px-6">
+          <p className="p3 text-primary">{fontName} 님</p>
+          <p className="p5 text-grey">제작 폰트 수: {count} 개</p>
+        </div>
+      </div>
+
+      <div className="mt-5 flex justify-end gap-7">
+        <button className="p5 text-grey">정보 수정</button>
+        <button className="p5 text-warn">로그아웃</button>
+      </div>
+    </SectionLayout>
+  )
+}
