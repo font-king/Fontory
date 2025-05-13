d5422dac06962469899acf39284ae6da290cc595
DongGyun <wooheuy5752@gmail.com>
2025-05-13 14:40:15 +0900

    chore: svg.d.ts 수정

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -2,3 +2,11 @@ declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGElement>>
   export default content
 }
+
+declare module '*.svg?react' {
+  import type * as React from 'react'
+  import type { SVGProps } from 'react'
+
+  const ReactComponent: React.FC<SVGProps<SVGSVGElement>>
+  export default ReactComponent
+}
